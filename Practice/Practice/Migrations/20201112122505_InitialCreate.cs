24f0187e5e799e489eeaf63e7a3d80db0eed182f
hoangmanh1999 <54472253+hoangmanh1999@users.noreply.github.com>
2020-11-12 20:00:11 +0700

    up

---
+++ Practice/Practice/Migrations/20201112122505_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Practice.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Year = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ Practice/Practice/Models/SeedData.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Practice.Models
+{
+    public class SeedData
+    {
+        public static void SeedEmployee(IApplicationBuilder app)
+        {
+            StoreDbContext context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<StoreDbContext>();
+
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            if (!context.Employees.Any())
+            {
+                context.Employees.AddRange(
+                        new Employee
+                        {
+                            Name = "Manh",
+                            Address = "Ha Noi",
+                            Phone = "111111111",
+                            Email = "manh@gmail.com",
+                            Year = new DateTime(1999,10,22)
+                        },
+                        new Employee
+                        {
+                            Name = "Manh2",
+                            Address = "Ha Noi",
+                            Phone = "222222222",
+                            Email = "manh2@gmail.com",
+                            Year = new DateTime(1999, 10, 22)
+                        },
+                        new Employee
+                        {
+                            Name = "Manh3",
+                            Address = "Ha Noi",
+                            Phone = "33333333",
+                            Email = "manh3@gmail.com",
+                            Year = new DateTime(1999, 10, 22)
+                        },
+                        new Employee
+                        {
+                            Name = "Manh4",
+                            Address = "Ha Noi",
+                            Phone = "44444444",
+                            Email = "manh4@gmail.com",
+                            Year = new DateTime(1999, 10, 22)
+                        },
+                        new Employee
+                        {
+                            Name = "Manh5",
+                            Address = "Ha Noi",
+                            Phone = "555555555",
+                            Email = "manh5@gmail.com",
+                            Year = new DateTime(1999, 10, 22)
+                        }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
